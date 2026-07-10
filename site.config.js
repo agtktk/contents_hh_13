2d9868b58d9b4b6584c2899c87c005d116c555bb
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 09:53:31 +0900

    start

---
+++ site.config.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  siteName: '알뜰폰정보이용료 희망티켓',
+  siteUrl: 'https://contents-hh-13.pages.dev',
+  siteDescription: '알뜰폰정보이용료 전문 상담. 전 알뜰폰 통신사 제한 없이 간편 인증만으로 실시간 신속 해결이 가능합니다.',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/sample.png',
+  naverVerificationCode: '',
+  googleVerificationCode: '',
+  company: '희망티켓',
+  phone: '01094691051',
+  sms: '01094691051',
+  kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
+};
