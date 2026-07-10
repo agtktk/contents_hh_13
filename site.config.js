6e022b709ce2ffcb2478a49df9fc88ff9ca44d04
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 11:50:15 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ export const siteConfig = {
   siteDescription: '알뜰폰정보이용료 전문 상담. 전 알뜰폰 통신사 제한 없이 간편 인증만으로 실시간 신속 해결이 가능합니다.',
   author: '희망티켓 010-9469-1051',
   defaultImage: '/images/sample.png',
-  naverVerificationCode: '',
+  naverVerificationCode: '53c1d9ab831ae3f82b6f802d24ef27dcd687dc54',
   googleVerificationCode: '',
   company: '희망티켓',
   phone: '01094691051',
